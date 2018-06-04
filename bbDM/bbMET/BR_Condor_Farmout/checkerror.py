bd4c276099006f13e8ca5c1d77a4eb370c08bdf5
Deepak Kumar <dekumar@Deepaks-iMac.local>
2018-06-05 00:50:06 +0530

    modified error file reader

--- bbDM/bbMET/BR_Condor_Farmout/checkerror.py
+++ bbDM/bbMET/BR_Condor_Farmout/checkerror.py
@@ -7,12 +7,26 @@ files=sorted(glob.glob(path))
 
 counts=[]
 
+#for file in files:
+    #f=open(file,'r')
+    #if (f.readlines())[-1]==None:
+            #print (f.readlines())
+    #last_line=f.readlines()[-1]
+    #if not str(last_line.split()[0])=='Completed.':
+            #print "======Please check this cluster Id(Not finished) in error directory"
+            #print file.split('/')[-1]
+            #counts.append(1)
+
+#print "Total errors:  ", len(counts)
+
 for file in files:
-    f=open(file,'r')
-    last_line=f.readlines()[-1]
-    if not str(last_line.split()[0])=='Completed.':
-        print "======Please check this cluster Id(Not finished) in error directory"
-        print file.split('/')[-1]
-        counts.append(1)
+    with open(file) as f:
+        lines = filter(None, (line.rstrip() for line in f))
+        if lines:
+            last_line=lines[-1]#.readlines()[-1]
+            if not str(last_line.split()[0])=='Completed.':
+                print "======Please check this cluster Id file.This file has some error====="
+                print file.split('/')[-1]
+                counts.append(1)
 
 print "Total errors:  ", len(counts)
